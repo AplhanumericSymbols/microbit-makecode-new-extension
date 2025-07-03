a07ed901afd2efb7cbb3e389bf0e9b663ca4e983
AplhanumericSymbols <chukwunyame@gmail.com>
2025-07-03 16:07:15 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+basic.showString("Ready")
+
+input.onButtonPressed(Button.A, function () {
+    voicebuffer.startRecording(3000)
+    basic.showIcon(IconNames.Yes)
+})
+
+input.onButtonPressed(Button.B, function () {
+    let clip = voicebuffer.getLastClip()
+    radio.sendBuffer(clip)
+})
